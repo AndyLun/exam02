f0659b9fa95fe68feb98ef57f4e05ca2f5e7eb59
AndyLun <andylun2009@gmail.com>
2020-04-29 16:32:10 +0800

    test.

--- main.cpp
+++ main.cpp
@@ -113,13 +113,14 @@ void logAccel()
 	float ay = (t[1] - v_down[1]) * 9.8;
 	float az = (t[2] - v_down[2]) * 9.8;
 
-	if(sqrt(pow(t[0] * v_down[0], 2) + pow(t[1] * v_down[1], 2) + pow(t[2] * v_down[2], 2)) < 0.1) {
+	pc.printf("%1.4f\r\n", sqrt(pow(t[0] * v_down[0], 2) + pow(t[1] * v_down[1], 2) + pow(t[2] * v_down[2], 2)));
+		/*if(sqrt(pow(t[0] * v_down[0], 2) + pow(t[1] * v_down[1], 2) + pow(t[2] * v_down[2], 2)) < 0.1) {
 		disp_h1 += ax * 0.005;
 		disp_h2 += ay * 0.005;
 
 		if(abs(disp_h1) > 0.05 || abs(disp_h2) > 0.05) horizbuf[bi] = 1;
 		else horizbuf[bi] = 0;
-	}
+	}*/
 
 	bi++;
 }
